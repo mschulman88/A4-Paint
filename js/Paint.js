2a4f20bf1751455363cbfe72858ab1be87b0e142
Mike <michael.schulman88@gmail.com>
2018-11-08 17:11:57 +0200

    JS Test

--- js/Paint.js
+++ js/Paint.js
@@ -1,1 +1,28 @@
-console.log("hello")
+var Paint = {};
+
+Paint.start = function(){
+    Paint.bindMenuActions();
+};
+
+Paint.bindMenuActions = function(){
+    var newBtn = document.getElementById("new");
+    newBtn.addEventListener("click", Paint.new);
+    var saveBtn = document.getElementById("save");
+    saveBtn.addEventListener("click", Paint.save);
+    var loadBtn = document.getElementById("load");
+    loadBtn.addEventListener("click", Paint.load);
+};
+
+Paint.new = function(){
+    alert("new");
+};
+
+Paint.save = function(){
+    alert("save");
+};
+
+Paint.load = function(){
+    alert("load");
+};
+
+Paint.start();
